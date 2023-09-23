ee31b284b4b37fd278fa3df2c161b1dd6d91907b
h-yasha <husssaindesu@gmail.com>
2023-09-23 10:53:59 +0300

    :sparkles: Add Language Server Loader/Config: lspconfig and Mason

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,45 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
+
+		require("mason").setup()
+
+		mason_lspconfig.setup()
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				print(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+					on_attach = function(_, bufnr)
+						local nmap = function(keys, func, desc)
+							if desc then
+								desc = "LSP: " .. desc
+							end
+
+							vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+						end
+						-- [[ Set Keymaps ]]
+						nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+						nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+						nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+						nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+						nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+						nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+						nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+						vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
+							vim.lsp.buf.format()
+						end, { desc = "Format current buffer with LSP" })
+					end,
+				})
+			end,
+		})
+	end,
+}
