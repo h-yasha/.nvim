8e14d6b0e556a0f3f8a78c6f85675b1b6534b384
h-yasha <husssaindesu@gmail.com>
2023-09-27 22:05:32 +0300

    :sparkles: Add treesitter plugins textobjects and context highlighter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
@@ -7,6 +11,14 @@ return {
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+				},
+			},
 		})
+
+		require("treesitter-context").setup()
 	end,
 }
