82c61c4f7e655f4630fd1a5ac9f553315b74aa46
h-yasha <husssaindesu@gmail.com>
2023-09-28 09:00:30 +0300

    :sparkles: Add diagnostics keymaps

--- init.lua
+++ init.lua
@@ -71,6 +71,11 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up and center" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next match and center" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous match and center" })
 
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
 -- [[ Highlight on yank ]]
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
