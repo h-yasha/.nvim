f4e43446dda6e314cfd6b4e9d549a36c9a1edc72
h-yasha <husssaindesu@gmail.com>
2023-09-29 14:55:27 +0300

    :sparkles: Add function signarure completion helper

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,6 +7,8 @@ return {
 
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+
+		"hrsh7th/cmp-nvim-lsp-signature-help",
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -52,6 +54,7 @@ return {
 				{ name = "nvim_lsp" },
 				{ name = "buffer" },
 				{ name = "path" },
+				{ name = "nvim_lsp_signature_help" },
 			},
 			view = { docs = { auto_open = true }, entries = { name = "custom" } },
 		})
