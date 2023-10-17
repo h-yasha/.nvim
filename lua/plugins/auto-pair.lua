431f5425928ddbd1acb6c37d8f350391e051a629
h-yasha <husssaindesu@gmail.com>
2023-10-17 21:26:44 +0300

    :sparkles: Switch to mini.pairs for auto pairs

--- lua/plugins/auto-pair.lua
+++ lua/plugins/auto-pair.lua
@@ -1,5 +1,20 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
+	"echasnovski/mini.pairs",
+	event = "VeryLazy",
 	opts = {},
+	keys = {
+		{
+			"<leader>up",
+			function()
+				local Util = require("lazy.core.util")
+				vim.g.minipairs_disable = not vim.g.minipairs_disable
+				if vim.g.minipairs_disable then
+					Util.warn("Disabled auto pairs", { title = "Option" })
+				else
+					Util.info("Enabled auto pairs", { title = "Option" })
+				end
+			end,
+			desc = "Toggle auto pairs",
+		},
+	},
 }
