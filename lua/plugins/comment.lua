5bd216ca79c2b67ed874b11d5606d421b0a98782
h-yasha <husssaindesu@gmail.com>
2023-10-17 21:33:03 +0300

    :sparkles: switch comments to mini.comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,21 @@
 return {
-	"numToStr/Comment.nvim",
-	opts = {},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
+	},
 }
